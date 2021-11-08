aa62ead9732b0865a7c7d3c2da423e7c15b36edc
mf-Mii <mf-Mii@outlook.jp>
2021-11-09 00:04:19 +0900

    Fix a critical bug

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const server = app.listen(3000, function(){
   console.log("Server booted! Now working to PORT"+server.address().port);
 });
 
+const db = new sqlite3.Database("./redirect.db");
 
 app.get("/:target", function(req,res,next){
   let t = "/"+req.params.target;
@@ -31,7 +32,6 @@ app.get("/system/:t", function(req,res,next){
   
   if(t=="setup"&&allowSetup){
     console.log("Setup now...");
-    let db = new sqlite3.Database("./redirect.db");
     db.serialize(()=>{
       db.run("CREATE TABLE IF NOT EXISTS `redirect`(`from` VARCHAR(200) NOT NULL, `to` VARCHAR(250) NOT NULL , `tmp` BOOLEAN NOT NULL DEFAULT '0', PRIMARY KEY (`from`));");
     });
@@ -40,6 +40,7 @@ app.get("/system/:t", function(req,res,next){
     console.log("Setup success");
   }else if(t=="status"){
     res.send("ok");
+    console.log("Status OK");
   }else{
     redirectApi("/"+t,res);
   }
@@ -54,7 +55,6 @@ app.post("/system/:a", function(req,res,next){
     let p = req.body.pass;
     console.log("POST /system/add-page "+f+" -> "+t+"("+tm+")");
     if(p===process.env.PASS_ADD){
-      let db = new sqlite3.Database("./redirect.db");
       db.serialize(()=>{
         let stmt = db.prepare("SELECT COUNT(*) AS cnt FROM `redirect` WHERE `from`=?;");
         let exists = false;
@@ -71,14 +71,12 @@ app.post("/system/:a", function(req,res,next){
           }
         });
       });
-      db.close();
     }
   }else if(a=="delete-page"){
     let f = req.body.from;
     let p = req.body.pass;
     console.log("POST /system/delete-page "+f);
     if(p===process.env.PASS_DELETE) {
-      let db = new sqlite3.Database("./redirect.db");
       db.serialize(()=>{
         let exists = false;
         let stmt = db.prepare("SELECT COUNT(*) AS cnt FROM `redirect` WHERE `from`=?;");
@@ -94,8 +92,7 @@ app.post("/system/:a", function(req,res,next){
             res.json({"status":"success"});
           }
         });
-      });
-      db.close();
+      })
     }
   }
 });
@@ -105,48 +102,44 @@ function redirect(from,res){
   let to = null;
   let tmp = -1;
   
-  let db = new sqlite3.Database("./redirect.db");
   db.serialize(()=>{
     let stmt = db.prepare("SELECT * FROM `redirect` WHERE `from`=?");
     stmt.each(from, function(err,row){
-      if(rows){
+      if(row){
         to = row.to;
         tmp = row.tmp;
       }
     });
 
     stmt.finalize();
+    console.log("Redirect To:"+to+"("+tmp+")");
+    if (to!=null){
+      res.redirect(tmp==1?302:301, to);
+    }else{
+      console.log("The url is not registered!");
+      res.redirect(302, "https://mfmii.work/404?s=redirect");
+    }
   });
-  db.close();
-  console.log("Redirect To:"+to+"("+tmp+")");
-  if (to!=null){
-    res.redirect(tmp==1?302:301, to);
-  }else{
-    console.log("The url is not registered!");
-    res.redirect(302, "https://mfmii.work/404?s=redirect");
-  }
 }
 
 function redirectApi(from,res){
   let to = null;
   let tmp = 0;
-  let db = new sqlite3.Database("./redirect.db");
   db.serialize(()=>{
     let stmt = db.prepare("SELECT * FROM `redirect` WHERE `from`=?");
     stmt.each(from, function(err,row){
-      if(rows){
+      if(row){
         to = row.to;
         tmp = row.tmp;
       }
     });
 
     stmt.finalize();
+    console.log("Redirect API:"+to+"("+tmp+")");
+    if (to!=null){
+      res.json({"from":from,"to":to,"temp_302":tmp==1?true:false});
+    }else{
+      res.json({"from":from,"to":null});
+    }
   });
-  db.close();
-  console.log("Redirect API:"+to+"("+tmp+")");
-  if (to!=null){
-    res.json({"from":from,"to":to,"temp_302":tmp==1?true:false});
-  }else{
-    res.json({"from":from,"to":null});
-  }
 }
